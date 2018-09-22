bca37c9b881fed01dd98508e339ab96c7f166f07
Glib Briia <glib.briia@gmail.com>
2018-09-22 14:54:33 +0100

    Add glue for examples

--- examples/src/test/java/calc/RunCukesTest.java
+++ examples/src/test/java/calc/RunCukesTest.java
@@ -1,8 +1,10 @@
 package calc;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(glue = {"src/test/groovy"})
 public class RunCukesTest {
 }
