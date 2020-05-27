1aa76cac447f72f3abb8c842d3fef80420cc998f
Glib Briia <glib.briia@gmail.com>
2020-05-27 10:38:30 +0100

    Remove unnecessary output

--- groovy/src/test/groovy/io/cucumber/groovy/steps.groovy
+++ groovy/src/test/groovy/io/cucumber/groovy/steps.groovy
@@ -17,7 +17,7 @@ Author singleAuthorTransformer(DataTable table) {
 }
 
 @DataTableType
-Author authorEntryTransformer(Map<String, String> entry) {
+static Author authorEntryTransformer(Map<String, String> entry) {
     return new Author(
             entry.get("firstName"),
             entry.get("lastName"),
@@ -36,15 +36,12 @@ Given("a single author in a table") {
 
 
 Given("a list of people in a table") { List<Person> persons ->
-    println(persons)
-    println(expectedPerson)
-    print(mononymousPerson)
     assertTrue(persons.contains(expectedPerson))
     assertTrue(persons.contains(mononymousPerson))
 }
 
 @DataTableType(replaceWithEmptyString = "[blank]")
-Person transformPerson(Map<String, String> tableEntry) {
+static Person transformPerson(Map<String, String> tableEntry) {
     return new Person(tableEntry.get("first"), tableEntry.get("last"))
 }
 
