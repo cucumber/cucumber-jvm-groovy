f78e2a85a10eef883fed1c6527b2767b2a96e3b5
Glib Briia <glib.briia@gmail.com>
2020-05-27 15:25:21 +0100

    Autoupdate version for maven and gradle

--- scripts/update-install-doc.sh
+++ scripts/update-install-doc.sh
@@ -3,7 +3,7 @@ set -uf -o pipefail
 
 # Reads installation doc from STDIN and writes out a new one to STDOUT where:
 #
-# * the version number is updated for both Maven and sbt
+# * the version number is updated for both Maven and Gradle
 #
 
 new_version=$1
@@ -12,8 +12,8 @@ installdoc=$(</dev/stdin)
 
 # Maven
 installdoc=$(echo "${installdoc}" | sed "s/<version>[0-9]\+.[0-9]\+.[0-9]\+<\/version>/<version>${new_version}<\/version>/g")
-# sbt
-installdoc=$(echo "${installdoc}" | sed "s/% \"[0-9]\+.[0-9]\+.[0-9]\+\" %/% \"${new_version}\" %/g")
+# Gradle
+installdoc=$(echo "${installdoc}" | sed "s/version: '[0-9]\+.[0-9]\+.[0-9]\+'/ '${new_version}'/g")
 
 # Output
 echo "${installdoc}"
