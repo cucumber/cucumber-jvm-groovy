512fea68c34c82a008cd80980970959dfcfdfacf
M.P. Korstanje <rien.korstanje@gmail.com>
2025-08-19 03:13:39 +0200

    Prefer GherkinDialects.getDialects()

--- groovy/src/main/groovy/generate-keywords.groovy
+++ groovy/src/main/groovy/generate-keywords.groovy
@@ -1,14 +1,12 @@
 import groovy.text.SimpleTemplateEngine
-import io.cucumber.gherkin.GherkinDialectProvider
+import io.cucumber.gherkin.GherkinDialects
 
 def engine = new SimpleTemplateEngine()
 def templateSource = new File(project.baseDir, "${File.separator}src${File.separator}main${File.separator}groovy${File.separator}I18n.groovy.gsp").getText()
 
 def unsupported = ["EM"] // The generated files for Emoij do not compile.
-def dialectProvider = new GherkinDialectProvider()
 
-dialectProvider.getLanguages().each { language ->
-    def dialect = dialectProvider.getDialect(language).orElseThrow()
+GherkinDialects.getDialects().each { dialect ->
     def normalized_language = dialect.language.replaceAll("[\\s-]", "_").toUpperCase()
     if (!unsupported.contains(normalized_language)) {
         def binding = ["i18n":dialect, "normalized_language":normalized_language]
